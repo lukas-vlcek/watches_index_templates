a5bb0eeab8c1b9c04f75738621381e0b3e4df532
Lukáš Vlček <lukas.vlcek@gmail.com>
2017-03-27 16:59:11 +0200

    Add dynamic section into defaults mappings
    Add `watches_` prefix to templates and indices

--- push_mappings.sh
+++ push_mappings.sh
@@ -4,5 +4,6 @@ ES=http://localhost:9200
 
 #curl -X DELETE $ES/*
 
-curl -X POST $ES/_template/cluster_health-2-4 -d@cluster_health_2-4-X.template.json
-curl -X POST $ES/_template/cluster_stats-2-4  -d@cluster_stats_2-4-X.template.json
+curl -X POST $ES/_template/watches_defaults_2-4        -d@watches_defaults_2-4-X.template.json
+curl -X POST $ES/_template/watches_cluster_health_2-4  -d@watches_cluster_health_2-4-X.template.json
+curl -X POST $ES/_template/watches_cluster_stats_2-4   -d@watches_cluster_stats_2-4-X.template.json
