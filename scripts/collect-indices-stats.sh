0d79db7d7141cb4a1e4377c2e82f4ae9f4bed3bd
Lukáš Vlček <lukas.vlcek@gmail.com>
2017-04-03 12:06:55 +0200

    Add indices stats
    - index template
    - fluentd conf
    - polling job

---
+++ scripts/collect-indices-stats.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+source _configure_env.sh
+
+TTL=${1:-60}
+
+echo "Polling indices_stats to ${PATH_WATCHES_INDICES_STATS_LOGS}"
+
+watches indices_stats \
+  -d $TTL -i 3 \
+  -sblt \
+  --level=shards \
+  --transform=nested \
+  --url=${ES_URL} \
+> ${PATH_WATCHES_NODES_STATS_LOGS}
--- scripts/push_templates.sh
+++ scripts/push_templates.sh
@@ -8,5 +8,6 @@ curl -X POST ${ES_URL}/_template/watches_defaults_2-4        -d@${INDEX_TEMPLATE
 curl -X POST ${ES_URL}/_template/watches_cluster_health_2-4  -d@${INDEX_TEMPLATES_DIR}/watches_cluster_health_2-4-X.template.json
 curl -X POST ${ES_URL}/_template/watches_cluster_stats_2-4   -d@${INDEX_TEMPLATES_DIR}/watches_cluster_stats_2-4-X.template.json
 curl -X POST ${ES_URL}/_template/watches_nodes_stats_2-4     -d@${INDEX_TEMPLATES_DIR}/watches_nodes_stats_2-4-X.template.json
+curl -X POST ${ES_URL}/_template/watches_indices_stats_2-4   -d@${INDEX_TEMPLATES_DIR}/watches_indices_stats_2-4-X.template.json
 
 echo ""
