d80e29b6952a069c9e0eb3f3f9c4273d6fdb9dc8
Lukáš Vlček <lukas.vlcek@gmail.com>
2017-06-15 11:11:40 -0400

    Push templates to ES on startup

--- run.sh
+++ run.sh
@@ -5,5 +5,6 @@ cd ./scripts/
 echo "Is ES available at ${ES_SCHEME}://${ES_HOST}:${ES_PORT} ?"
 curl -s -X GET ${ES_SCHEME}://${ES_HOST}:${ES_PORT}
 
+./push_templates.sh
 ./start_fluentd.sh
 ./start_watches.sh -1
