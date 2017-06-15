677818802ad85e7f75344ef2c63b4da521c303de
Lukáš Vlček <lukas.vlcek@gmail.com>
2017-06-15 13:08:31 -0400

    Fix the command

--- scripts/init_kibana_indices.sh
+++ scripts/init_kibana_indices.sh
@@ -5,8 +5,7 @@ source _configure_env.sh
 wait_for_port_open
 
 # Get all .kibana* indices
-curl -s -X GET ${ES_URL}/_cat/indices/.kibana*?h=index
-indices=$(`curl -s -X GET ${CURL_CERT_OPTIONS} ${ES_URL}/_cat/indices/.kibana*?h=index`)
+indices=$(curl -s -X GET ${CURL_CERT_OPTIONS} ${ES_URL}/_cat/indices/.kibana*?h=index)
 
 echo ${indices}
 for index in "${indices[@]}"
