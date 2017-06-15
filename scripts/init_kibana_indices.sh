56910618b57ed7026045ea208295d363e3a58815
Lukáš Vlček <lukas.vlcek@gmail.com>
2017-06-15 13:50:13 -0400

    Add logging prints

--- scripts/init_kibana_indices.sh
+++ scripts/init_kibana_indices.sh
@@ -15,6 +15,8 @@ do
   echo Insert dashboards into ${index} index
   data=`cat ${metadata_path}/.kibana_bulk_data_without_config`
   echo ${data//\"_index\":\".kibana\"/\"_index\":\"${index}\"} > ${metadata_path}/.kibana_bulk_data_without_config_specific
-  curl -v -s -X POST ${ES_URL}/_bulk?pretty --data-binary @${metadata_path}/.kibana_bulk_data_without_config_specific
+  echo "****************"
+  cat "@${metadata_path}/.kibana_bulk_data_without_config_specific"
+  curl -v -s -X POST ${ES_URL}/_bulk?pretty --data-binary "@${metadata_path}/.kibana_bulk_data_without_config_specific"
   rm ${metadata_path}/.kibana_bulk_data_without_config_specific
 done
