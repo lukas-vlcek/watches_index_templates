6d5782eb598b841f8e09f05ecc3a6d3481521eaa
Lukáš Vlček <lukas.vlcek@gmail.com>
2017-06-15 13:03:08 -0400

    Get all kibana indices

--- scripts/init_kibana_indices.sh
+++ scripts/init_kibana_indices.sh
@@ -3,3 +3,12 @@
 source _configure_env.sh
 
 wait_for_port_open
+
+# Get all .kibana* indices
+indices=$(`curl -s -X GET ${CURL_CERT_OPTIONS} ${ES_URL}/_cat/indices/.kibana*?h=index`)
+
+echo ${indices}
+for index in "${indices[@]}"
+do
+  echo ${index}
+done
